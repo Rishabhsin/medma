1fd77dc2ce5fe92318b0aa639c489048c3554335
Rishabhsin <111456553+Rishabhsin@users.noreply.github.com>
2022-11-11 00:02:16 +0530

    Update doctorsinup.js

--- controllers/doctorsinup.js
+++ controllers/doctorsinup.js
@@ -12,3 +12,39 @@ export const createDoctor= async(req,res)=>{
         throw(error);
     }
 }
+export const updatedoctor = async(req, res, next) =>{
+    try {
+        const updatedoctor = await doctor.findByIdAndUpdate(req.params.id,{$set: req.body},{new:true});
+        res.status(200).json(updatedoctor);
+
+    } catch (error) {
+        next(error);        
+    }
+}
+
+export const deletedoctor = async(req, res, next)=>{
+    try {
+        await doctor.findByIdAndDelete(req.params.id);
+        res.status(200).json("doctor successfully deleted");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getdoctor = async (req, res, next) => {
+    try {
+        const doctor = await doctor.findById(req.params.id)
+        res.status(200).json(doctor);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const getdoctors = async (req, res, next) => {
+    try {
+        const doctors = await doctor.find(req.params.id);
+        res.status(200).json(doctors);
+    } catch (err) {
+        next(err)
+    }
+}
