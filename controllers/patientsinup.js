ac06f12a005cfa10766d71df7c02b56a64358bd6
Rishabhsin <111456553+Rishabhsin@users.noreply.github.com>
2022-11-11 00:04:59 +0530

    Update patientsinup.js

--- controllers/patientsinup.js
+++ controllers/patientsinup.js
@@ -10,8 +10,24 @@ export const createPatient = async(req,res)=>{
     {
         throw(error);
     }
+}
 
+export const updatePatient= async (req, res, next) => {
+    try {
+        const updatePatient = await Patient.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(updatePatient);
+    } catch (error) {
+            next(error);
+        }
+}
 
+export const deletePatient= async(req, res, next) =>{
+    try { 
+        await Patient.findByIdAndDelete(req.params.id);
+        res.status(200).jso("Delete Patient Successful");
 
-
+    } catch (error) {
+        next(err)
+        
+    }
 }
