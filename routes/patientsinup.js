b0d3b682802adc784d6663ef1662e1ab72b05b9a
Rishabhsin <111456553+Rishabhsin@users.noreply.github.com>
2022-11-17 00:41:04 +0530

    Update patientsinup.js

--- routes/patientsinup.js
+++ routes/patientsinup.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createPatient , updatePatient, deletePatient} from "../controllers/patientsinup.js";
+import { createPatient , updatePatient, deletePatient, getpatient, getpatients} from "../controllers/patientsinup.js";
 
 const router = express.Router();
 
 
 router.post('/', createPatient);
 router.put('/:id', updatePatient);
-router.delete('/:id', deletePatient)
+router.delete('/:id', deletePatient);
+router.get('/:id', getpatient);
+router.get('/', getpatients);
 
 export default router;
