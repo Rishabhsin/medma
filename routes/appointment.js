275ea9c059279fdce7ab14f89f3664548cc051e3
Rishabh Singh <111456553+Rishabhsin@users.noreply.github.com>
2022-11-18 02:36:28 +0530

    Update appointment.js

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,5 +1,5 @@
 import express from "express"; 
-import { createAppointment, deleteappointment, getappointment, getappointments, updateAppointment } from "../controllers/appointment.js";
+import { createAppointment, deleteappointment, getappointment, getappointments, updateAppointment ,getappointmentbydoc} from "../controllers/appointment.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.put('/:id', updateAppointment);
 router.delete('/:id', deleteappointment);
 router.get('/:id', getappointment);
 router.get('/', getappointments);
+router.get('/app/:id',getappointmentbydoc);
 
 export default router;
